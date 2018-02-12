c4fb7f2968d0d658bd15dbe332f7e25152f455bd
bikrambhattacharya <getbikram1@gmail.com>
2018-02-12 14:49:39 +0530

    SSR using razzle

---
+++ build/0.39a57a7534aa2977815e.hot-update.js
@@ -0,0 +1,53 @@
+exports.id = 0;
+exports.modules = {
+
+/***/ "./src/App.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("react");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_router_dom_Route__ = __webpack_require__("react-router-dom/Route");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_react_router_dom_Route___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1_react_router_dom_Route__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_router_dom_Switch__ = __webpack_require__("react-router-dom/Switch");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_react_router_dom_Switch___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_2_react_router_dom_Switch__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__Home__ = __webpack_require__("./src/Home.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__About__ = __webpack_require__("./src/About.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__App_css__ = __webpack_require__("./src/App.css");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__App_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_5__App_css__);
+var _jsxFileName = "/home/bikram/server-react/src/App.js";
+
+
+
+
+
+
+
+var App = function App() {
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(
+    __WEBPACK_IMPORTED_MODULE_2_react_router_dom_Switch___default.a,
+    {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 9
+      }
+    },
+    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_react_router_dom_Route___default.a, { exact: true, path: "/", component: __WEBPACK_IMPORTED_MODULE_3__Home__["a" /* default */], __source: {
+        fileName: _jsxFileName,
+        lineNumber: 10
+      }
+    }),
+    __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1_react_router_dom_Route___default.a, { exact: true, path: "/about", component: __WEBPACK_IMPORTED_MODULE_4__About__["a" /* default */], __source: {
+        fileName: _jsxFileName,
+        lineNumber: 11
+      }
+    })
+  );
+};
+
+/* harmony default export */ __webpack_exports__["a"] = (App);
+
+/***/ })
+
+};
+//# sourceMappingURL=0.39a57a7534aa2977815e.hot-update.js.map
